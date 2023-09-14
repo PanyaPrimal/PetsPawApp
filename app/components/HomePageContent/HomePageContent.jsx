7cdcbbd045a2d46aa647362d4a2522a046d0ef04
Victor <vpanya97@gmail.com>
2023-09-14 15:31:05 +0300

    fix background responsibiliy bug

--- app/components/HomePageContent/HomePageContent.jsx
+++ app/components/HomePageContent/HomePageContent.jsx
@@ -3,23 +3,26 @@ import Image from 'next/image';
 
 export const HomePageContent = () => {
   return (
-    <div className='
-      hidden
-      lg:flex
-      w-[680px]
-      mb-[30px]
-      ml-auto
-      bg-primary-soft dark:bg-black-soft   
-      rounded-[20px]
-    '>
-      <div className='h-full ml-[560px] absolute right-0 top-0 overflow-hidden'>
-        <Image
-          src='/assets/images/girl-and-pet1.png'
-          width={775}
-          height={900}
-          alt='Home page image, girl with the cat'
-        />
+      <div className='
+        hidden
+        lg:flex
+      '>        
+        <div className='h-full ml-[560px] absolute right-0 top-0 overflow-hidden'>
+          <div className='
+            w-[calc(100%-60px)] h-[calc(100vh-60px)] absolute top-[30px]
+            ml-[30px] 
+            bg-primary-soft dark:bg-black-soft
+            rounded-[20px]
+            z-[-1]
+          '>
+          </div>
+          <Image
+            src='/assets/images/girl-and-pet1.png'
+            width={775}
+            height={900}
+            alt='Home page image, girl with the cat'
+          />
+        </div>
       </div>
-    </div>
   );
 };

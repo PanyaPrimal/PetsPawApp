16dcc334678a938a0ae68762a1ad3a4c195bcd29
Victor P <vpanya97@gmail.com>
2023-10-11 10:57:48 +0300

    fix useless import

--- app/store/store.jsx
+++ app/store/store.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userLogSlice from "./reducers/userLogSlice";
 import burgerSlice from "./reducers/burgerSlice";
 
 const store = configureStore({
   reducer: {
-    userLog: userLogSlice,
     burger: burgerSlice,
   },
 });
